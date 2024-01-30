5d2adfd713b92b35d2e0dc27ea57c147de160844
Anand P <anandpamegoze@gmail.com>
2024-01-30 19:46:47 +0530

    create loading component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Container className="mt-5">
+          <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
